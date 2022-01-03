fd80cb97ba518e88b5af89075544af8141ebf6ca
xieTP <xtp1994@gmail.com>
2022-01-03 12:37:12 +0800

    add config files

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,19 @@
+alias ls='ls -G --color=always'               # colorize `ls` output
+alias ll='ls -l'                              # colorize `ls` output
+alias la='ls -la'                             # colorize `ls` output
+alias zshreload='source ~/.zshrc'             # reload ZSH
+alias shtop='sudo htop'                       # run `htop` with root rights
+alias grep='grep --color=auto'                # colorize `grep` output
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias less='less -R'
+alias g='git'
+
+alias rm='rm -i'                              # confirm removal
+alias cp='cp -i'                              # confirm copy
+alias mv='mv -i'                              # confirm move
+alias cal='gcal --starting-day=1'             # print simple calendar for current month
+alias weather='curl v2.wttr.in'               # print weather for current location (https://github.com/chubin/wttr.in)
+
+alias man='tldr'
